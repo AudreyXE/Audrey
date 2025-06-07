dd8230eed9d23bec9819c4280968e6f2430dda16
AudreyXE <audreyaudreykhaira@gmail.com>
2025-06-07 16:01:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,5 @@ import streamlit as st
 st.title("Selamat Datang di Web Informatika")
 st.write(
     "Ngoding bersama bapak Hendri"
-)
-
-st.image("https://github.com/AudreyXE/Audrey/blob/main/IMG-20250527-WA0020.jpg , width = 200)
-
+st.write("IMG-20250527-WA0020.jpg , width = 200)
          Aplikasi sederhana
